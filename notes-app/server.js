e024646fc49c30a2757f319b31768b4eb8cccc80
Thecanadianchris <thecanadianchris@yahoo.com>
2026-07-08 11:58:08 +0100

    server.js / package.jason / node server

---
+++ notes-app/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("notes app is alive");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
